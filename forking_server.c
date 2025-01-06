e56a377dcbd48fb95a26bb9545b5e056a407b6fe
Ray Xu <rxu50@stuy.edu>
2025-01-06 15:34:54 -0500

    Trying to work out forking server

---
+++ forking_server.c
@@ -0,0 +1,73 @@
+#include "pipe_networking.h"
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+
+int to_client;
+int from_client;
+int server_running = 1;
+
+void sigint_handler(int signo) {
+    if (signo == SIGINT) {
+        printf("\nServer shutting down gracefully...\n");
+        unlink(WKP);
+        server_running = 0;
+        if (to_client > 0) {
+            int sig = -1;
+            write(to_client, &sig, sizeof(sig));
+        }
+        exit(0);
+    }
+}
+
+int main() {
+    signal(SIGINT, sigint_handler);
+    signal(SIGPIPE, SIG_IGN);
+
+    while (server_running) {
+        printf("Waiting for a new client...\n");
+        from_client = server_setup();
+        pid_t p = fork();
+        if (p<0) {
+            printf("ERROR FORKING\n");
+            return 1;
+        }    
+        else if (p==0) { 
+            if (from_client == -1 || to_client == -1) {
+                printf("Error during handshake. Continuing to next client...\n");
+                continue;
+            }
+            printf("Client connected. Sending...\n");
+            srand(time(NULL));
+            sleep(1);
+            while (1) { 
+                int randnum = rand() % 101; 
+                printf("Sending: %d\n", randnum);
+                int test = write(to_client, &randnum, sizeof(randnum));
+                //printf("%d\n", test);
+                if (test == -1) {
+                    printf("Error writing to client\n");
+                    break;
+                }
+                sleep(1);
+                int recv_sig;
+                if (read(from_client, &recv_sig, sizeof(recv_sig)) > 0 && recv_sig == -1) {
+                    printf("Client sent termination signal. Disconnecting...\n");
+                    break;
+                }
+            }
+        }
+        else {
+            continue;
+        }
+        
+        /*
+        printf("Client disconnected\n");
+        close(from_client);
+        close(to_client);
+        */
+    }
+    return 0;
+}
--- makefile
+++ makefile
@@ -3,14 +3,14 @@ all: client server
 client: persistant_client.o pipe_networking.o
 	gcc -o client persistant_client.o pipe_networking.o
 
-server: persistant_server.o pipe_networking.o
-	gcc -o server persistant_server.o pipe_networking.o
+server: forking_server.o pipe_networking.o
+	gcc -o server forking_server.o pipe_networking.o
 
 persistant_client.o: persistant_client.c pipe_networking.h
 	gcc -c persistant_client.c
 
-persistant_server.o: persistant_server.c pipe_networking.h
-	gcc -c persistant_server.c
+forking_server.o: forking_server.c pipe_networking.h
+	gcc -c forking_server.c
 
 pipe_networking.o: pipe_networking.c pipe_networking.h
 	gcc -c pipe_networking.c
